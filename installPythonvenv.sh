3d5f1dcbf5b2b5e069b84c674d9520752ced1985
Roelof Rietbroek <r.rietbroek@utwente.nl>
2024-02-21 17:13:23 +0100

    try out with older version of geemap

--- installPythonvenv.sh
+++ installPythonvenv.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/bash
 
 # create a python virtual environment in the user's home
-python -m venv ${HOME}/private/pyhydra
 
-${HOME}/private/pyhydra/bin/pip install -r requirements-buildenv.txt 
+hydrapath=${HOME}/pyhydra
+python -m venv $hydrapath 
+
+${hydrapath}/bin/pip install -r requirements-buildenv.txt 
 
 
 # make the python kernel available to jupyter lab (note: requires a restart of jupyter lab to make it available)
 
-${HOME}/private/pyhydra/bin/python -m ipykernel install --user --name pyhydra
+${hydrapath}/bin/python -m ipykernel install --user --name pyhydra
+
+
+
+# install hydriafloods requirements
+
+${hydrapath}/bin/pip install -r requirements-hydrafloods.txt 
+
 
 # export all (working requirements)
 # ${HOME}/pyhydra/bin/pip freeze > requirements.txt
